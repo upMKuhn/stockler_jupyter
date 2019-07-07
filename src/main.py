ffc47c714933a1ba68c70fe971a52cedc5074d29
Martin Kuhn <martin.kuhn@trivago.com>
2019-07-07 23:25:49 +0200

    sync

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
-from pprint import pprint
 
-from utils.stockler_influx import get_stockler_data_client
-
+import pandas as pd
+from models import train_price_ai as modeling
 
 def main():
-    client = get_stockler_data_client()
-    result = client.query(""" 
-       SELECT mean("average_price") AS mean_average_price FROM stockler.autogen.stocks 
-       WHERE "symbol"='XBIO' GROUP BY time(30m) FILL(none)
-    """.strip())
+    data_set_files = [
+        'symbol__TSLA_2018-08-02_2019-04-30',
+        'symbol__XBIO_2018-08-02_2019-04-30'
+    ]
+    validation_percent = 0.25
+    data_sets = {file: pd.read_csv(f'../data/{file}.csv') for file in data_set_files}
+    for data in data_sets.values():
+        data.index = pd.to_datetime(data.index)
+        symbol = list(data_sets.keys())[0]
+        print(symbol)
+        trainer = modeling.PriceAiTrainer(data_sets)
+        model = trainer.build_model()
+        history = trainer.train(model, symbol)
 
-    client.close()
-    return result
-
-
+main()
--- src/models/train_price_ai.py
+++ src/models/train_price_ai.py
@@ -1,19 +1,80 @@
+import sys
+from collections import defaultdict
+
+import numpy as np
+from pandas import DataFrame
 from tensorflow.python.keras import Sequential
 from tensorflow.python.keras.layers import LSTM, Dense
+from tensorflow.python.keras.engine import training
+from typing import Dict, Tuple
 
-look_back = 40
-forward_days = 10
-num_periods = 20
 
-NUM_NEURONS_FirstLayer = 128
-NUM_NEURONS_SecondLayer = 64
-EPOCHS = 220
+class PriceAiTrainer:
 
-# Build the model
-model = Sequential()
-model.add(LSTM(NUM_NEURONS_FirstLayer, input_shape=(look_back, 1), return_sequences=True))
-model.add(LSTM(NUM_NEURONS_SecondLayer, input_shape=(NUM_NEURONS_FirstLayer, 1)))
-model.add(Dense(forward_days))
-model.compile(loss='mean_squared_error', optimizer='adam')
-history = model.fit(X_train, y_train, epochs=EPOCHS, validation_data=(X_validate, y_validate), shuffle=True,
-                    batch_size=2, verbose=2)
+    def __init__(
+            self,
+            data_by_symbol: Dict[str, DataFrame],
+            look_back=40,
+            forward_prediction=40,
+            test_size_percent=0.25,
+            validation_column='close'
+    ):
+        self.sample_size = look_back * 2
+        self.validation_column = validation_column
+        self.test_size_percent = test_size_percent
+        self.look_back = look_back
+        self.forward_prediction = forward_prediction
+        self.data_by_symbol = data_by_symbol
+        self.num_features = 0
+        self.validate_data_by_symbol = defaultdict()
+        self.train_data_by_symbol = defaultdict()
+
+        if len(data_by_symbol.keys()):
+            self.num_features = len(data_by_symbol[list(data_by_symbol.keys())[0]].columns) - 1
+            self.__split_train_and_test_data()
+
+    def build_model(self, neurons_first_layer=None, neurons_second_layer=None):
+        neurons_first_layer = neurons_first_layer or self.look_back * self.num_features
+        neurons_second_layer = neurons_second_layer or int(neurons_first_layer / 2)
+
+        model = Sequential()
+        model.add(
+            LSTM(neurons_first_layer, input_shape=(self.look_back, self.num_features), return_sequences=True)
+        )
+        model.add(LSTM(neurons_second_layer, input_shape=(neurons_first_layer, 1)))
+        model.add(Dense(self.forward_prediction))
+        return model
+
+    def get_training_data(self, symbol):
+        return self.train_data_by_symbol[symbol]
+
+    # Build the model
+    def train(self, model: training.Model, symbol, epochs=220):
+        x_train, y_train = self.train_data_by_symbol[symbol]
+        x_validate, y_validate = self.validate_data_by_symbol[symbol]
+        model.compile(loss='mean_squared_error', optimizer='adam')
+        history = model.fit(
+            x_train, y_train, epochs=epochs, validation_data=(x_validate, y_validate), shuffle=True,
+            verbose=2
+        )
+        return model, history
+
+    def reshape_data(self, data: DataFrame):
+
+        num_items = len(data)
+        items_to_drop = int((num_items % self.look_back))
+        train_data = data.copy()[items_to_drop:]
+        validation_data = data[[self.validation_column]].copy()[items_to_drop:]
+
+        print(items_to_drop)
+
+        x_data = np.array(np.split(train_data.to_numpy()[:, 1:], len(train_data) / 40))
+        y_data = np.array(np.split(validation_data.to_numpy().flatten(), len(validation_data) / 40))
+
+        return x_data, y_data
+
+    def __split_train_and_test_data(self):
+        for symbol, data in self.data_by_symbol.items():
+            test_size = int(self.test_size_percent * len(data))
+            self.train_data_by_symbol[symbol] = self.reshape_data(data[:-test_size])
+            self.validate_data_by_symbol[symbol] = self.reshape_data(data.tail(test_size))
